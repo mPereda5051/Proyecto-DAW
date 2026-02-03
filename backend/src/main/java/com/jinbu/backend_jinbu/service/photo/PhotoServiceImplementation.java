23bd5466d1d45f04f3aa59f94f66887afe4f1b8a
martdbj <181728462+martdbj@users.noreply.github.com>
2026-02-03 21:24:16 +0100

    Comentario PhotoService

--- backend/src/main/java/com/jinbu/backend_jinbu/service/photo/PhotoServiceImplementation.java
+++ backend/src/main/java/com/jinbu/backend_jinbu/service/photo/PhotoServiceImplementation.java
@@ -7,5 +7,10 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @Service
 public class PhotoServiceImplementation {
+    /*
+    El servicio PhotoService se va a tener que comunicar con un S3 de AWS para almacenar las fotos de 
+    manera efectiva sin dañar al servidor.
+    (La IA dice que una buena manera es con procesamiento asíncrono con AWS Lambda, pero habrá que investigar)
     
+    */
 }
