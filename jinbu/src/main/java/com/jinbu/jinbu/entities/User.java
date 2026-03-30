696ea9ee7bb430de633603ad096aad12c5c7ee51
mPereda5051 <98544413+mPereda5051@users.noreply.github.com>
2026-03-30 02:26:11 +0200

    creado docker file para el backend y frontend. Cambio en los puertos para evitar conflictos 8090 para back y 3001 para front.  Correccion de un error de mapeo en entity/User y centralizacion de la configuracion de docker en la raiz de proyecto

--- jinbu/src/main/java/com/jinbu/jinbu/entities/User.java
+++ jinbu/src/main/java/com/jinbu/jinbu/entities/User.java
@@ -53,6 +53,6 @@ public class User {
     private Set<User> followers = new HashSet<>();
 
     @JsonIgnore
-    @OneToMany(mappedBy = "users", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Post> posts;
 }
